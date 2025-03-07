4f9dcf27fe8ce6bbb02aaa872d8b9d7c946ba18b
BheemaReddy07 <bheemareddy29102003@gmail.com>
2025-03-07 16:54:17 +0530

    partially completed

--- controllers/userController.js
+++ controllers/userController.js
@@ -36,23 +36,23 @@ const requestOTPtoRegister = async (req, res) => {
   try {
     const { name, email, password, repassword } = req.body;
     if (!name || !email || !password) {
-      return res.json({ success: false, message: "all fields required" });
+      return res.status(400).json({ success: false, message: "all fields required" });
     }
     if (!validator.isEmail(email)) {
-      return res.json({ success: false, message: "Enter valid email" });
+      return res.status(400).json({ success: false, message: "Enter valid email" });
     }
     if (password.length < 8) {
-      return res.json({ success: false, message: "enter strong password" });
+      return res.status(400).json({ success: false, message: "enter strong password" });
     }
     if (password !== repassword) {
-      return res.json({
+      return res.status(400).json({
         success: false,
         message: "password doesnot match with repassword",
       });
     }
     const user = await userModel.findOne({ email });
     if (user && user.verified) {
-      return res.json({ success: false, message: "User already exists" });
+      return res.status(400).json({ success: false, message: "User already exists" });
     }
 
     const otp = generateOTP();
@@ -76,10 +76,10 @@ const requestOTPtoRegister = async (req, res) => {
     }
 
     await sendOTPEmail(name, email, otp);
-    res.json({ success: true, message: "otp sent successfully" });
+    res.status(200).json({ success: true, message: "otp sent successfully" });
   } catch (error) {
     console.log(error);
-    res.json({ success: false, message: error.message });
+    res.status(500).json({ success: false, message: error.message });
   }
 };
 
@@ -111,81 +111,173 @@ const verifyOTPandRegister = async (req, res) => {
   }
 };
 
-
-const loginUser = async (req,res) =>{
-    try {
-        const {email,password} = req.body
-        const user = await userModel.findOne({email})
-        if(!user){
-            return res.json({success:false,message:"No user found"})
-        }
-
-        const isMatch =  await bcrypt.compare(password,user.password)
-
-        if(isMatch){
-            const token = jwt.sign({id:user._id},process.env.JWT_SECRET)
-            res.json({success:true,message:"Login successfull",token})
-        }
-        else{
-            return res.json({success:false,message:"incorrect password"})
-        }
-    } catch (error) {
-        console.log(error);
-        res.json({ success: false, message: error.message });
-    }
-}
-
-const requestForgotPasswordOtp = async (req, res) => {
+const loginUser = async (req, res) => {
   try {
-    const {name,email} = req.body
-    const user = await userModel.findOne({email})
-    if(!user){
-        return res.json({success:false,message:"No user found"})
+    const { email, password } = req.body;
+    const user = await userModel.findOne({ email });
+    if (!user) {
+      return res.json({ success: false, message: "No user found" });
     }
-    const otp = generateOTP();
-    console.log(otp)
-    user.otp = otp;
-    user.otpExpiration = Date.now()+3*60*1000;
 
-    await user.save();
+    const isMatch = await bcrypt.compare(password, user.password);
 
-    await sendOTPEmail(name,email,otp);
-
-    res.json({success:true,message:"otp send for RESET password"})
-
+    if (isMatch) {
+      const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+      res.json({ success: true, message: "Login successfull", token });
+    } else {
+      return res.json({ success: false, message: "incorrect password" });
+    }
   } catch (error) {
     console.log(error);
     res.json({ success: false, message: error.message });
   }
 };
 
-const resetPassword = async (req,res) =>{
+const requestForgotPasswordOtp = async (req, res) => {
+  try {
+    const { name, email } = req.body;
+    const user = await userModel.findOne({ email });
+    if (!user) {
+      return res.json({ success: false, message: "No user found" });
+    }
+    const otp = generateOTP();
+    console.log(otp);
+    user.otp = otp;
+    user.otpExpiration = Date.now() + 3 * 60 * 1000;
+
+    await user.save();
+
+    await sendOTPEmail(name, email, otp);
+
+    res.json({ success: true, message: "otp send for RESET password" });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+const resetPassword = async (req, res) => {
+  try {
+    const { email, password, repassword, otp } = req.body;
+    const user = await userModel.findOne({ email });
+    if (!user) {
+      return res.json({ success: false, message: "no user found" });
+    }
+    if (user.otp !== otp || Date.now() > user.otpExpiration) {
+      return res.json({ success: false, message: "Invalid or Expired otp" });
+    }
+    if (password !== repassword) {
+      return res.json({
+        success: false,
+        message: "password not matching with repassword",
+      });
+    }
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    user.otp = null;
+    user.password = hashedPassword;
+    user.otp = null;
+    await user.save();
+
+    res.json({ success: true, message: "password reset successfull" });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+const cleanUpExpiredUser = async () => {
+  try {
+    const now = Date.now();
+    const expiredUsers = await userModel.deleteMany({
+      verified: false,
+      otpExpiration: { $lt: now },
+    });
+    console.log(`Deleted ${expiredUsers.deletedCount} expired users.`);
+  } catch (error) {
+    console.error("Error cleaning up expired users:", error.message);
+  }
+};
+
+const time = 5 * 60 * 1000;
+setInterval(cleanUpExpiredUser, time);
+
+const getAllUsers = async (req, res) => {
+  try {
+    const users = await userModel
+      .find({ verified: true })
+      .select(["-password", "-verified", "-otp", "-otpExpiration"]);
+    res.json({ success: true, users });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+const updateUserDetails = async (req, res) => {
+  try {
+    const { userId, name, age, phone, degree, branch, college } = req.body;
+
+    const updatedData = {};
+    if (name) updatedData.name = name;
+    if (age) updatedData.age = age;
+    if (phone) updatedData.phone = phone;
+    if (degree) updatedData.degree = degree;
+    if (branch) updatedData.branch = branch;
+    if (college) updatedData.college = college;
+
+    await userModel.findByIdAndUpdate(userId, updatedData, { new: true });
+
+    res.json({ success: true, message: "details updated" });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+
+
+const getProfileDetails = async (req,res) =>{
     try {
-        const {email,password,repassword,otp} = req.body
-        const user  = await userModel.findOne({email})
+        const {userId} = req.body
+        const user = await userModel.findById(userId).select(["-password", "-verified", "-otp", "-otpExpiration"])
         if(!user){
-            return res.json({success:false,message:"no user found"})
+            return res.status(400).json({success:false,message:"No user Found"})
         }
-        if(user.otp!==otp || Date.now()>user.otpExpiration){
-            return res.json({success:false,message:"Invalid or Expired otp"})
-        }
-        if(password!==repassword){
-            return res.json({success:false,message:"password not matching with repassword"})
-        }
-        const salt = await bcrypt.genSalt(10)
-        const hashedPassword = await bcrypt.hash(password,salt)
-
-        user.otp =null;
-        user.password=hashedPassword;
-        user.otp = null;
-        await user.save()
-
-        res.json({success:true,message:"password reset successfull"})
-
+        res.status(200).json({success:true,user,message:"userData fetched successfully"})
     } catch (error) {
         console.log(error);
-        res.json({ success: false, message: error.message });
+    res.status(500).json({ success: false, message: error.message });
     }
 }
 
-export { requestOTPtoRegister, verifyOTPandRegister,loginUser,requestForgotPasswordOtp ,resetPassword};
+
+
+
+
+const deleteUser = async (req,res) =>{
+    try {
+        const {userId} = req.body
+
+        const deletedUser = await userModel.findByIdAndDelete(userId)
+        if(!deletedUser){
+            return res.status(400).json({success:false,message:"No user Found"})
+        }
+        res.json({success:true,message:"user deleted successfully"})
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ success: false, message: error.message });
+    }
+}
+export {
+  requestOTPtoRegister,
+  verifyOTPandRegister,
+  loginUser,
+  requestForgotPasswordOtp,
+  resetPassword,
+  getAllUsers,
+  updateUserDetails,
+  getProfileDetails,
+  deleteUser
+};
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,5 +1,6 @@
 import express from 'express'
-import { loginUser, requestForgotPasswordOtp, requestOTPtoRegister, resetPassword, verifyOTPandRegister } from '../controllers/userController.js';
+import { deleteUser, getAllUsers, getProfileDetails, loginUser, requestForgotPasswordOtp, requestOTPtoRegister, resetPassword, updateUserDetails, verifyOTPandRegister } from '../controllers/userController.js';
+import authUser from '../middleware/authUser.js';
 
 
 const userRouter = express();
@@ -10,5 +11,18 @@ userRouter.post("/verify-register",verifyOTPandRegister)
 userRouter.post("/login",loginUser)
 userRouter.post("/forgot-otp",requestForgotPasswordOtp)
 userRouter.post("/reset",resetPassword)
+
+
+
+userRouter.get("/get-all-users",getAllUsers)
+userRouter.get("/profile",authUser,getProfileDetails)
+
+
+userRouter.put("/update-details",authUser,updateUserDetails)
+
+
+userRouter.delete("/delete",authUser,deleteUser)
+
+
 
 export default userRouter
